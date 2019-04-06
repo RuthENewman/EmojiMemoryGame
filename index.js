be5727f7c98720c40c3c820b0142c2404147d0e4
Ruth Newman <ruthnewman@ruths-air.home>
2019-04-06 23:13:12 +0100

    game now working with refactored code and cards shuffled on each load with initializer. no header or could add more cards to add to project

--- index.js
+++ index.js
@@ -1,7 +1,62 @@
 const memoryCards = document.querySelectorAll('.memory-card');
 
+let cardIsFlipped = false;
+let lockedBoard = false;
+let firstCard
+let secondCard;
+
 memoryCards.forEach(card => card.addEventListener('click', flipCard))
 
-function flipCard() {
-  this.classList.toggle('flip');
+function initialize() {
+  shuffle();
 }
+
+
+function flipCard() {
+  if (lockedBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!cardIsFlipped) {
+    cardIsFlipped = true;
+    firstCard = this;
+    return;
+  }
+    secondCard = this;
+    doCardsMatch();
+}
+
+function doCardsMatch() {
+  let cardsMatch = firstCard.dataset.emoji === secondCard.dataset.emoji
+  cardsMatch ? disableCards() : unflipCards()
+}
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+  resetCards();
+}
+
+function unflipCards() {
+  lockedBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetCards();
+  }, 1000)
+}
+
+function resetCards() {
+  cardIsFlipped = false;
+  lockedBoard = false;
+  firstCard = null
+  secondCard = null;
+}
+
+(function shuffle() {
+  memoryCards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 12)
+    card.style.order = randomPosition;
+  });
+})();
